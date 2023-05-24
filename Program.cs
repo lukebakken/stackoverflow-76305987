332e5488a9a41d6adc4474e754d2460583edfea2
avonsyr <134529916+avonsyr@users.noreply.github.com>
2023-05-24 17:38:44 +0200

    Added files

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Security.Authentication;
+using System.Diagnostics;
+using System.Net.Security;
+
+
+namespace MQTTTest
+{
+    static class Program
+    {       
+        static void Main()
+        {
+            RabbitMQ.Client.ConnectionFactory _factory;
+            RabbitMQ.Client.IConnection _connection;
+            try
+            {
+                _factory = new ConnectionFactory { 
+                    HostName = "some.url.com",
+                    Port = 5671, 
+                    UserName = "guest", 
+                    Password = "guest", 
+                    VirtualHost = "/",
+                    AutomaticRecoveryEnabled = true,
+                    NetworkRecoveryInterval = TimeSpan.FromSeconds(10),
+                    ContinuationTimeout = TimeSpan.FromSeconds(10),
+                    RequestedHeartbeat = TimeSpan.FromSeconds(10)
+                };
+
+                _factory.Ssl.Enabled = true;
+                _factory.Ssl.Version = SslProtocols.Tls12;
+                _factory.Ssl.AcceptablePolicyErrors = 
+                    SslPolicyErrors.RemoteCertificateChainErrors |
+                    SslPolicyErrors.RemoteCertificateNameMismatch |
+                    SslPolicyErrors.RemoteCertificateNotAvailable;
+
+                _connection  = _factory.CreateConnection();
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine("Error while connection to RabbitMQ Broker: " + e.Message);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                return;
+            }
+
+
+            var _channel  = _connection.CreateModel();
+
+            _channel .ExchangeDeclare(exchange: "exchangekey", 
+                            type: ExchangeType.Topic);     
+            var _queueName  = _channel .QueueDeclare().QueueName;     
+
+            _channel .QueueBind(queue: _queueName ,
+                      exchange: "exchangekey",
+                      routingKey: "routingkey");                       
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var _consumer = new EventingBasicConsumer(_channel );
+            _consumer.Received += (model, ea) =>
+            {
+                var _body = ea.Body.ToArray();
+                var _message = Encoding.UTF8.GetString(_body);
+                var _routingKey = ea.RoutingKey;
+                Console.WriteLine($" [x] Received '{_routingKey}':'{_message}'");
+            };
+            _channel.BasicConsume(queue: _queueName ,
+                                 autoAck: true,
+                                 consumer: _consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
